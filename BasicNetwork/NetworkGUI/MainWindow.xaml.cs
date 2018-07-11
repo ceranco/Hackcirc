7e3015b1ec1b2c6a28598d56d4ee74c2b56ad600
ceranco <ceranco@gmail.com>
2018-07-11 23:56:26 +0300

    Saving on Side

--- BasicNetwork/NetworkGUI/MainWindow.xaml.cs
+++ BasicNetwork/NetworkGUI/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace NetworkGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Queue<string> _packetQueue = new Queue<string>(10);
         public string ID { get; set; } = "1";
 
         public string MissionStatus { get; set; } = "Active";
@@ -27,6 +28,20 @@ namespace NetworkGUI
         public MainWindow()
         {
             InitializeComponent();
+
+            _packetQueue.Enqueue("Test Message");
+
+
+            PacketTextBox.AppendText("Test Message1\n");
+            PacketTextBox.SelectAll();
+            PacketTextBox.SelectionBrush = Color;
+            int firstCharOfLineIndex = PacketTextBox.();
+            int currentLine = richTextBox1.GetLineFromCharIndex(firstCharOfLineIndex);
+            this.myRichTextBox.Select(firstCharOfLineIndex, currentLine);
+            this.myRichTextBox.SelectionBackColor = Color.Aqua;
+            this.myRichTextBox.Select(0, 0);
+
+            PacketTextBox.AppendText("Test Message2\n");
         }
     }
 }
