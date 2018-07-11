7b53f3bad5a5735e2a172debe264f8e08bd806ed
michaelkats <41107180+michaelkats@users.noreply.github.com>
2018-07-11 14:12:57 +0300

    Test

---
+++ Test/ConsoleApp1/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Message
+    {
+        private byte[] data = null;
+
+        public byte[] Data { get => data; set => data = value; }
+
+        public Message(byte[] _data)
+        {
+            data = _data;
+        }        
+    }
+}
