8655350c88fdd3190e9331364c7c73c633a23fab
michaelkats <41107180+michaelkats@users.noreply.github.com>
2018-07-11 23:26:13 +0300

    2.7

--- BasicNetwork/BasicNetwork/Program.cs
+++ BasicNetwork/BasicNetwork/Program.cs
@@ -12,7 +12,7 @@ namespace BasicNetwork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Program Version 2.6\n");
+            Console.WriteLine("Program Version 2.7\n");
 
             int count = 0;
             Node node = new Node();
