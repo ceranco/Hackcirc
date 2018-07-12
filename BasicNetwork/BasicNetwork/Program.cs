ddd33dbce19cd8d204093869fe9f82b9ecd1d370
michaelkats <41107180+michaelkats@users.noreply.github.com>
2018-07-12 08:32:58 +0300

    2.20

--- BasicNetwork/BasicNetwork/Program.cs
+++ BasicNetwork/BasicNetwork/Program.cs
@@ -14,7 +14,7 @@ namespace BasicNetwork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Program Version 2.19\n");
+            Console.WriteLine("Program Version 2.20\n");
             string pictureName = "BW.bmp";
             //string pictureName = "team2.bmp";
 
@@ -25,51 +25,58 @@ namespace BasicNetwork
 
             Node node = null;
 
-            if (imageExists)
+            try
             {
-                Bitmap bmp = new Bitmap(pictureName);
 
-                MemoryStream ms = new MemoryStream();
-                // Save to memory using the BMP format
-                bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
-
-                // read to end
-                bmpBytes = ms.GetBuffer();
-                bmp.Dispose();
-                ms.Close();
-
-                node = new Node(bmpBytes.Length, true);
-            }
-            else
-            {
-                node = new Node(0, false);
-            }
-
-            if (node.Id == 1)
-            {                
-                int size = 100;
-                int index = 0;
-                Console.WriteLine("\nTransferring Image Started\n");
-                while (index <= bmpBytes.Length)
+                if (imageExists)
                 {
-                    byte[] result = new byte[size];
+                    Bitmap bmp = new Bitmap(pictureName);
 
-                    int length = Math.Min((bmpBytes.Length - index), result.Length);
+                    MemoryStream ms = new MemoryStream();
+                    // Save to memory using the BMP format
+                    bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
 
-                    Buffer.BlockCopy(bmpBytes, index,
-                        result, 0, length);
-                    index += size;
+                    // read to end
+                    bmpBytes = ms.GetBuffer();
+                    bmp.Dispose();
+                    ms.Close();
 
-                    node.SendInfo(result, length, 0);
-
-                    Thread.Sleep(500);
+                    node = new Node(bmpBytes.Length, true);
                 }
-                Console.WriteLine("\nTransferring Image Completed\n");
-            }
+                else
+                {
+                    node = new Node(0, false);
+                }
 
-            while(true)
-            {
-                Thread.Sleep(10000);
+                if (node.Id == 1)
+                {
+                    int size = 100;
+                    int index = 0;
+                    Console.WriteLine("\nTransferring Image Started\n");
+                    while (index <= bmpBytes.Length)
+                    {
+                        byte[] result = new byte[size];
+
+                        int length = Math.Min((bmpBytes.Length - index), result.Length);
+
+                        Buffer.BlockCopy(bmpBytes, index,
+                            result, 0, length);
+                        index += size;
+
+                        node.SendInfo(result, length, 0);
+
+                        Thread.Sleep(500);
+                    }
+                    Console.WriteLine("\nTransferring Image Completed\n");
+                }
+
+                while (true)
+                {
+                    Thread.Sleep(10000);
+                }
+            }
+            catch(Exception)
+            {                
             }
         }
     }
